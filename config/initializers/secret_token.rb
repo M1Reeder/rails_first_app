455718559e491aca654c67d5c803f744a92869c0
Michael Reeder <reeder@knoesis.org>
2012-07-16 14:04:45 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '8882f745d411263269059eac35cad6c92429bdcd72409a305db8fe70060d7621448ed6d359dc0b0ebd15872b5aeadaa2de054d1a9e0d50cf0874bfe1b9bfcbca'
